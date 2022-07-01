8c86f89a52ae4e1f360feaa492f1718d27685bd6
pelumini <pelumini@gmail.com>
2022-07-01 06:43:56 +0100

    stripe payment added

---
+++ src/utils/stripe-helpers.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-restricted-syntax */
+export const CURRENCY = 'usd';
+// Set your amount limits: Use float for decimal currencies and
+// Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
+export const MIN_AMOUNT = 10.0;
+export const MAX_AMOUNT = 5000.0;
+export const AMOUNT_STEP = 5.0;
+
+export function formatAmountForDisplay(
+  amount: number,
+  currency: string
+): string {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'symbol',
+  });
+  return numberFormat.format(amount);
+}
+
+export function formatAmountForStripe(
+  amount: number,
+  currency: string
+): number {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'symbol',
+  });
+  const parts = numberFormat.formatToParts(amount);
+  let zeroDecimalCurrency = true;
+  for (const part of parts) {
+    if (part.type === 'decimal') {
+      zeroDecimalCurrency = false;
+    }
+  }
+  return zeroDecimalCurrency ? amount : Math.round(amount * 100);
+}
+
+export function formatAmountFromStripe(
+  amount: number,
+  currency: string
+): number {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'symbol',
+  });
+  const parts = numberFormat.formatToParts(amount);
+  let zeroDecimalCurrency = true;
+  for (const part of parts) {
+    if (part.type === 'decimal') {
+      zeroDecimalCurrency = false;
+    }
+  }
+  return zeroDecimalCurrency ? amount : Math.round(amount / 100);
+}
